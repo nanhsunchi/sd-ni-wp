84890060fc3229944aa97dc837d2eb5fd277f011
Nan-Hsun Chi <nanhsunchi@gmail.com>
2025-02-19 17:44:56 -0800

    use correct LP82 Cd to re-calculate wind power input from 3 SDs (HU Lee case)

--- HU_LEE/test_coare3.6/test_coare36.m
+++ HU_LEE/test_coare3.6/test_coare36.m
@@ -1,16 +1,17 @@
 clear; close all; clc
 restoredefaultpath
-% addpath(genpath('~/Documents/MATLAB/COARE3.6/'))
-addpath('~/Documents/projects/sd-ni-wp/HU_LEE/')
+addpath(genpath('~/Documents/MATLAB/COARE3.6/'))
+% addpath('~/Documents/projects/sd-ni-wp/HU_LEE/')
 
 %%
 clc
-u = 30; % mean wind speed accounting for the ocean current vector
+u = 25:0.1:28; % mean wind speed accounting for the ocean current vector
 zu = 3.5;
 test = NaN*ones(size(u(:)));
 test1 = ones(size(u(:)));
-A = coare36vn_zrf_et(u(:),zu,27*test1,zu,90*test1,zu,1000*test1,29*test1,500*test1,400*test1,...
-    NaN,NaN,NaN,NaN,test,35*test1,test,test,10*test1,10*test1,10*test1);
+A = coare36vn_zrf_et(u(:),zu,27+randn(size(test)),zu,80+randn(size(test)),zu,...
+    1000+randn(size(test)),28+randn(size(test)),500+randn(size(test)),NaN,...
+    NaN,NaN,NaN,NaN,test,34+randn(size(test)),NaN,NaN,10,10,10);
 % plot
 close
 % subplot(1,2,1)
